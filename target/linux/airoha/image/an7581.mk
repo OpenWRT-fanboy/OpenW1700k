d7b9d1843dca546f066a3b3f0278d80a1781ba92
fanboy <fanboy@openwrt.org>
2026-02-23 14:08:11 -1000

    use 7.7v3 NPU firmware and disable CONFIG_NET_AIROHA_FLOW_STATS
    When the CONFIG_NET_AIROHA_FLOW_STATS was enabled, the output of
    
        cat /sys/kernel/debug/ppe/bind
    
    was empty. That is wrong, because even when it is done on the
    dumb ap device, where bridger is installed, some records
    should be there.
    After disabling CONFIG_NET_AIROHA_FLOW_STATS, the command contains
    entries so it seems that the NPU feature works normally now with
    device with mt7996.
    
    Signed-off-by: Daniel Pawlik <pawlik.dan@gmail.com>

--- target/linux/airoha/image/an7581.mk
+++ target/linux/airoha/image/an7581.mk
@@ -53,7 +53,9 @@ define Device/gemtek_w1700k
   DEVICE_ALT1_MODEL := W1700K
   DEVICE_ALT2_VENDOR := Quantum Fiber
   DEVICE_ALT2_MODEL := W1700K
-  DEVICE_PACKAGES := kmod-i2c-an7581 kmod-hwmon-nct7802 \
+  DEVICE_DTS := an7581-w1700k
+  DEVICE_PACKAGES := airoha-en7581-mt7996-npu-firmware \
+		    kmod-i2c-an7581 kmod-hwmon-nct7802 \
 		    kmod-mt7996-firmware kmod-phy-realtek \
 		    rtl8261n-firmware wpad-basic-mbedtls
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
